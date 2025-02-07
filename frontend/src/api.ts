3ebdc5e747f80336cf9338846dcae63a0124035d
Danny Armijos <daniel2667@hotmail.es>
2025-02-07 02:16:37 +0100

    cambio de url de express backend

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const fetchData = async () => {
-  return await axios.get('http://159.223.250.23:3001/api/data');
+  return await axios.get('http://104.248.100.4:3001/api/data');
 };
